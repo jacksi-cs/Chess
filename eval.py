6c5055404a0a2de218f3460a0463abcb3560bdc1
Jack Si <jacksi@utexas.edu>
2021-08-18 18:07:12 -0500

    Hard reset checkpoint

--- eval.py
+++ eval.py
@@ -5,6 +5,7 @@ import chess
 import random
 from anytree import Node, RenderTree
 from stopwatch import Stopwatch
+import time
 
 counter = 0
 recurr_list = []
@@ -24,162 +25,102 @@ piece_value = {
     'k' : 0
 }
 
-# Constructs minimax tree recursively
+# Constructs game tree recursively; could improve time with 1D or bitboard representation
+generate_avg = [0,0]
+push_avg = [0,0]
+pop_avg = [0,0]
 def generate_tree(id, prnt, cbrd, mv, depth):
+    
+    global generate_avg
+    global push_avg
+    global pop_avg
+
     global counter
     global recurr_list
-    counter += 1
 
     if depth >= 0:
-        node = Node(id, parent = prnt, cboard=cbrd, move=mv, score=None)
+        counter += 1
+        stime = time.time()
+        node = Node(id, parent = prnt, cboard=cbrd, move=mv, score=None, bmove=None) # identification, parent of the node, cboard of the node, move of the node, score of the node, best move of its children
         id = 0
+        etime = time.time()
+        generate_avg[0] += etime-stime
+        generate_avg[1] += 1
         for move in cbrd.legal_moves:
+            stime = time.time()
             cbrd.push(move)
+            etime = time.time()
+            push_avg[0] += etime-stime
+            push_avg[1] += 1
             if (cbrd.board_fen(),cbrd.castling_rights) not in recurr_list:
                 generate_tree(id, node, chess.Board(cbrd.fen()), move, depth-1)
                 recurr_list.append((cbrd.board_fen(),cbrd.castling_rights))
+            stime = time.time()
             cbrd.pop()
+            etime = time.time()
+            pop_avg[0] += etime-stime
+            pop_avg[1] += 1
             id += 1
 
         if (prnt == None):
-            # print(counter)
             return node
 
-def recur_func(cboard, eval, node, turn):
+def recur_func(cboard, eval, node, turn, alpha, beta):
     if node.is_leaf:
-        # print(node)
         node.score = eval(node.cboard)
-        # print(eval_score)
-        return node.score
+        return node.score, node.move
     
     for child in node.children:
         if node.score == None:
-            eval_score = recur_func(cboard, eval, child, not turn)
-            node.score = eval_score
+            eval_score = recur_func(cboard, eval, child, not turn, alpha, beta)
+            node.score = eval_score[0]
+            node.bmove = eval_score[1]
         elif turn: # White's turn (max node)
-            eval_score = recur_func(cboard, eval, child, not turn)
-            if node.score < eval_score:
-                # print(eval_score)
-                node.score = eval_score
-        else: # Black's turn (min node)
-            eval_score = recur_func(cboard, eval, child, not turn)
-            if node.score > eval_score:
-                # print(eval_score)
-                node.score = eval_score
+            eval_score = recur_func(cboard, eval, child, not turn, alpha, beta)
 
-    return node.score
+            # alpha beta pruning
+            alpha = max(alpha, eval_score[0])
+            if beta <= alpha:
+                break
+
+            if node.score < eval_score[0]:
+                node.score = eval_score[0]
+                node.bmove = eval_score[1]
+        else: # Black's turn (min node)
+            eval_score = recur_func(cboard, eval, child, not turn, alpha, beta)
+
+            # alpha beta pruning
+            beta = min(beta, eval_score[0])
+            if beta <= alpha:
+                break
+
+            if node.score > eval_score[0]:
+                node.score = eval_score[0]
+                node.bmove = eval_score[1]
+
+    return node.score, node.move
 
 def minimax_recur(cboard, eval, depth):
+    stopwatch = Stopwatch()
+    stopwatch.start()
     global recurr_list
+    global counter
+
+    global generate_avg
+    global pop_avg
+    global push_avg
+
     recurr_list = [] # Emptying global cache of board states previously seen, find a better way to design this
     root = generate_tree(0, None, cboard, None, depth)
+    stopwatch.stop()
+    print(str(stopwatch))
+    stopwatch.start()
     # print(root, root.cboard, root.is_leaf)
-    recur_func(cboard, eval, root, cboard.turn)
-    # print(RenderTree(root))
-    
-    optimal_node = None
-    for child in root.children:
-        if optimal_node == None:
-            optimal_node = child
-        else:
-            if cboard.turn: # White, therefore maximize
-                if child.score > optimal_node.score:
-                    optimal_node = child
-            else: # Black, therefore minimize
-                if child.score < optimal_node.score:
-                    optimal_node = child
-    
-    print("optimal move: ", optimal_node.move, "score: ", optimal_node.score)
-    # print(optimal_node.children)
-    return cboard.san(optimal_node.move)
-
-
-###########################################
-
-def minimax(cboard, eval, depth):
-    stopwatch = Stopwatch()
-    root = generate_tree(0, None, cboard, None, depth)
+    recur_func(cboard, eval, root, cboard.turn, float('-inf'), float('inf'))
     stopwatch.stop()
-    # print("GENERATING TREE: ", str(stopwatch))
-    # print(RenderTree(root))
-
-    stopwatch.restart()
-
-    leaves_list = root.leaves
-    to_maximize = False if leaves_list[0].cboard.turn == chess.WHITE else True # It is flipped because this pertains to the layer above the leaf nodes
-    layer_list = [[]] # Each elem is a list representing a layer of the tree (starting from the 2nd to bottom)
-    optimal_move = None
-    optimal_score = None
-    parent_score = None
-    for leaf_node in leaves_list:
-        if leaf_node.parent not in layer_list[0]:
-            print("A: ", leaf_node.parent, leaf_node)
-            layer_list[0].append(leaf_node.parent)
-        eval_score = eval(leaf_node.cboard)
-        # print(leaf_node.move, eval_score, parent_score)
-        if leaf_node.parent.score == None: # Assuming that leaf is not the root (depth != 1), TODO: should add precondition
-            leaf_node.parent.score = eval_score
-            optimal_move = leaf_node.move
-            optimal_score = eval_score
-            # print(parent_score)
-        # Parents of the leaf nodes are trying to maximize the score
-        elif to_maximize:
-            if leaf_node.parent.score < eval_score:
-                # print(parent_score)
-                leaf_node.parent.score = eval_score
-                optimal_move = leaf_node.move
-                optimal_score = eval_score
-        # Parents of the leaf nodes are trying to minimize the score
-        elif leaf_node.parent.score > eval_score:
-            # print(parent_score)
-            leaf_node.parent.score = eval_score
-            optimal_move = leaf_node.move
-            optimal_score = eval_score
-        # print(parent_score)
-
-    index = 0
-    while True:
-        if root in layer_list[index]:
-            #print(layer_list[index], index)
-            break
-        else:
-            layer_list.append([])
-            to_maximize = not to_maximize
-            print(cboard.turn. to_maximize)
-            for node in layer_list[index]:
-                # if node == root:
-                #     print("done")
-                if node.parent not in layer_list[index+1]:
-                    layer_list[index+1].append(node.parent)
-                eval_score = eval(node.cboard)
-                parent_score = leaf_node.parent.score
-            if node.parent.score == None: # Assuming that leaf is not the root (depth != 1), TODO: should add precondition
-                node.parent.score = eval_score
-                optimal_move = leaf_node.move
-                optimal_score = eval_score
-            # Parents of the leaf nodes are trying to maximize the score
-            elif to_maximize:
-                if node.parent.score < eval_score:
-                    # print(parent_score)
-                    node.parent.score = eval_score
-                    optimal_move = leaf_node.move
-                    optimal_score = eval_score
-            # Parents of the leaf nodes are trying to minimize the score
-            elif node.parent.score > eval_score:
-                # print(parent_score)
-                node.parent.score = eval_score
-                optimal_move = leaf_node.move
-                optimal_score = eval_score
-
-    for children in root.children:
-        print(children.score, children.move)
-        
-    print("OPTIMAL MOVE: ", optimal_move, "SCORE: ", optimal_score)
-    stopwatch.stop()
-    # print("GENERATING MOVE: ", str(stopwatch))
-    if optimal_score == 0:
-        return random_move(cboard)
-    return cboard.san(optimal_move)
+    print(root.bmove, counter, str(stopwatch))
+    print("generate: ", generate_avg[0]/generate_avg[1], "push: ", push_avg[0]/push_avg[1], "pop: ", pop_avg[0]/pop_avg[1])
+    return cboard.san(root.bmove)
 
 # Picks the first move and returns an AN string
 def first_move(cboard):
@@ -195,7 +136,9 @@ counter=0
 # Naive evaluation function (piece capture, checkmate)
 # White trying to maximize, black is trying to minimize
 def naive_eval(cboard):
-    # print(cboard.board_fen)
+    white_count = 0
+    black_count = 0
+
     global counter
     counter += 1
     if cboard.is_checkmate():
@@ -205,9 +148,12 @@ def naive_eval(cboard):
             return 100
     if cboard.is_stalemate():
         return 0
+    if cboard.is_check():
+        if cboard.turn == chess.WHITE:
+            black_count += -5
+        elif cboard.turn == chess.BLACK:
+            white_count += 5
 
-    white_count = 0
-    black_count = 0
     board_fen = cboard.board_fen()
     for char in board_fen:
         if char.isupper():
@@ -218,7 +164,7 @@ def naive_eval(cboard):
     return white_count + black_count
 
 # # MAIN
-# board = chess.Board("rnb1qr1k/6bp/2p3pn/pp1pp3/5p1P/5N2/PPPPPPP1/RNBQKB1R w Q - 2 21")
+board = chess.Board("rnb1qr1k/6bp/2p3pn/pp1pp3/5p1P/5N2/PPPPPPP1/RNBQKB1R w Q - 2 21")
 board = chess.Board("rnbqkbnr/pppppppp/7N/8/8/8/PPPPPPPP/RNBQKB1R w KQkq - 0 1")
 
-minimax_recur(board, naive_eval, 2)
+minimax_recur(board, naive_eval, 3)
--- test.py
+++ test.py
@@ -1,8 +1,65 @@
+from re import S
 import chess
-board = chess.Board()
+import random
+from anytree import Node, RenderTree
+from stopwatch import Stopwatch
+import time
 
-move = chess.Move.from_uci("a2a4")
-board.push(move)
-test = board
-board.pop()
-print(test)
+
+generate_avg = [0,0]
+push_avg = [0,0]
+pop_avg = [0,0]
+recurr_list = []
+counter = 0
+noncounter = 0
+def generate_tree(id, prnt, cbrd, mv, depth):
+    stopwatch = Stopwatch()
+    global generate_avg
+    global push_avg
+    global pop_avg
+
+    global counter
+    global noncounter
+
+    global recurr_list
+
+    if depth >= 0:
+        counter += 1
+        stopwatch.start()
+        node = Node(id, parent = prnt, cboard=cbrd, move=mv, score=None, bmove=None) # identification, parent of the node, cboard of the node, move of the node, score of the node, best move of its children
+        id = 0
+        # print("generate: ", str(stopwatch))
+        for move in cbrd.legal_moves:
+            stime = time.time()
+            # stopwatch.restart()
+            cbrd.push(move)
+            # print("push: ", stopwatch)
+            # etime = time.time()
+            # push_avg[0] += etime-stime
+            # push_avg[1] += 1
+            if (cbrd.board_fen(),cbrd.castling_rights) not in recurr_list:
+                generate_tree(id, node, chess.Board(cbrd.fen()), move, depth-1)
+                recurr_list.append((cbrd.board_fen(),cbrd.castling_rights))
+            else:
+                noncounter += 1
+            # stime = time.time()
+            # stopwatch.restart()
+            cbrd.pop()
+            # print("pop: ", str(stopwatch))
+            # etime = time.time()
+            # pop_avg[0] += etime-stime
+            # pop_avg[1] += 1
+            id += 1
+
+        if (prnt == None):
+            counter += 1
+            return node
+
+cboard = chess.Board("rnbqkbnr/pppppppp/7N/8/8/8/PPPPPPPP/RNBQKB1R w KQkq - 0 1")
+stopwatch = Stopwatch()
+stopwatch.start()
+root = generate_tree(0, None, cboard, None, 4)
+stopwatch.stop()
+print(str(stopwatch), counter, noncounter, len(root.leaves))
+# print("generate: ", generate_avg[0]/generate_avg[1], "push: ", push_avg[0]/push_avg[1], "pop: ", pop_avg[0]/pop_avg[1])
+# print(RenderTree(root))
