dfc5db1d753afbc2b1b2378732993c0f26b626cf
Jack Si <53351286+utjacksi@users.noreply.github.com>
2021-08-23 18:54:29 -0500

    Added improved eval function, fully functional

--- eval.py
+++ eval.py
@@ -9,7 +9,7 @@ import time
 from board import Board
 
 counter = 0
-recurr_list = []
+recurr_list = {}
 
 piece_value = {
     'P' : 1,
@@ -17,161 +17,79 @@ piece_value = {
     'B' : 3,
     'R' : 5,
     'Q' : 9,
-    'K' : 0, # Kings will always be on the board
+    'K' : 0.5, # Kings will always be on the board
     'p' : -1,
     'n' : -3,
     'b' : -3,
     'r' : -5,
     'q' : -9,
-    'k' : 0
+    'k' : -0.5
 }
-
-# Constructs game tree recursively; could improve time with 1D or bitboard representation
-generate_avg = [0,0]
-push_avg = [0,0]
-pop_avg = [0,0]
-def generate_tree(id, prnt, cbrd, mv, depth):
-    
-    global generate_avg
-    global push_avg
-    global pop_avg
-
-    global counter
-    global recurr_list
-
-    if depth >= 0:
-        counter += 1
-        stime = time.time()
-        node = Node(id, parent = prnt, cboard=cbrd, move=mv, score=None, bmove=None) # identification, parent of the node, cboard of the node, move of the node, score of the node, best move of its children
-        id = 0
-        etime = time.time()
-        generate_avg[0] += etime-stime
-        generate_avg[1] += 1
-        for move in cbrd.legal_moves:
-            stime = time.time()
-            cbrd.push(move)
-            etime = time.time()
-            push_avg[0] += etime-stime
-            push_avg[1] += 1
-            if (cbrd.board_fen(),cbrd.castling_rights) not in recurr_list:
-                generate_tree(id, node, chess.Board(cbrd.fen()), move, depth-1)
-                recurr_list.append((cbrd.board_fen(),cbrd.castling_rights))
-            stime = time.time()
-            cbrd.pop()
-            etime = time.time()
-            pop_avg[0] += etime-stime
-            pop_avg[1] += 1
-            id += 1
-
-        if (prnt == None):
-            return node
-
-def recur_func(cboard, eval, node, turn, alpha, beta):
-    if node.is_leaf:
-        node.score = eval(node.cboard)
-        return node.score, node.move
-    
-    for child in node.children:
-        if node.score == None:
-            eval_score = recur_func(cboard, eval, child, not turn, alpha, beta)
-            node.score = eval_score[0]
-            node.bmove = eval_score[1]
-        elif cboard.turn: # White's turn (max node)
-            eval_score = recur_func(cboard, eval, child, not turn, alpha, beta)
-
-            # alpha beta pruning
-            alpha = max(alpha, eval_score[0])
-            if beta <= alpha:
-                break
-
-            if node.score < eval_score[0]:
-                node.score = eval_score[0]
-                node.bmove = eval_score[1]
-        else: # Black's turn (min node)
-            eval_score = recur_func(cboard, eval, child, not turn, alpha, beta)
-
-            # alpha beta pruning
-            beta = min(beta, eval_score[0])
-            if beta <= alpha:
-                break
-
-            if node.score > eval_score[0]:
-                node.score = eval_score[0]
-                node.bmove = eval_score[1]
-
-    return node.score, node.move
 
 
 def recur_func_2(node_info, max_depth, cbrd, eval, depth, alpha, beta): # node_info = [id, parent, move]
     node = Node(name=node_info[0], parent=node_info[1], cboard=cbrd, move=node_info[2], score=None, bmove=None)
-    # print(node.depth, node.cboard.turn)
 
-    if node.depth == max_depth or not any(cbrd.legal_moves):
+    if node.depth == max_depth or not any(cbrd.legal_moves): # Reached max depth, end of game, or leaf node due to truncation
         node.score = eval(node.cboard)
         return node.score, node.move
     else:
         counter = 0
-        if cbrd.can_claim_draw():
-            print("REEEEEEEEEEEEEEE")
 
         alpha = float('-inf')
         beta = float('inf')
+
+        "Attempt at removing repetitive game states, but only increased time"
+        recurr_list_elem = (cbrd.board_fen(), cbrd.castling_rights, node.depth)
+        in_recurr_list = True
+        if recurr_list_elem in recurr_list:
+            return recurr_list[recurr_list_elem]
+        else:
+            in_recurr_list = False
+
         for move in cbrd.legal_moves:
             cbrd.push(move)
-            cbrd_modified = chess.Board(cbrd.fen())
+            cbrd_modified = cbrd.copy()
+            # cbrd_modified = chess.Board(cbrd.fen())
             cbrd.pop()
+
+            eval_score = recur_func_2([counter, node, move], max_depth, cbrd_modified, eval, depth+1, alpha, beta)
+
             if node.score == None:
-                # print("a", node)
-                eval_score = recur_func_2([counter, node, move], max_depth, cbrd_modified, eval, depth+1, alpha, beta)
                 node.score = eval_score[0]
                 node.bmove = eval_score[1]
             elif cbrd.turn: # White's turn (max node)
-                eval_score = recur_func_2([counter, node, move], max_depth, cbrd_modified, eval, depth+1, alpha, beta)
-
                 # alpha beta pruning
-                # if eval_score[0] != None:
                 alpha = max(alpha, eval_score[0])
                 if beta <= alpha:
-                    print("PLEASE SEE")
+                    print("PRUNED")
                     counter += 1
                     break
 
-                # DEBUG
-                # if eval_score[0] == None:
-                #     print(eval_score, cbrd_modified, node.depth)
-                #     print([counter, node, move], max_depth, cbrd_modified, eval, depth+1, alpha, beta)
                 if node.score < eval_score[0]:
                     node.score = eval_score[0]
                     node.bmove = eval_score[1]
             else: # Black's turn (min node)
-                eval_score = recur_func_2([counter, node, move], max_depth, cbrd_modified, eval, depth+1, alpha, beta)
-
                 # alpha beta pruning
-                # if eval_score[0] != None:
                 beta = min(beta, eval_score[0])
                 if beta <= alpha:
-                    print("PLEASE SEE")
+                    print("PRUNED")
                     counter += 1
                     break
 
-
-                # DEBUG
-                # if eval_score[0] == None:
-                #     print(eval_score, cbrd_modified, node.depth)
-                #     print([counter, node, move], max_depth, cbrd_modified, eval, depth+1, alpha, beta)
                 if node.score > eval_score[0]:
                     node.score = eval_score[0]
                     node.bmove = eval_score[1]
             
             counter += 1
+
+        "Attempt at removing repetitive game states, but only increased time"
+        if not in_recurr_list:
+            recurr_list[recurr_list_elem] = (node.score, node.move)
             
         if node.depth == 0:
             return node.score, node.bmove, node
 
-
-        if node.score == None:
-            print(node)
-            print("YEH YOH")
         return node.score, node.move
         
 
@@ -181,7 +99,7 @@ def minimax_recur(board, eval):
     global recurr_list
     global counter
 
-    recurr_list = [] # Emptying global cache of board states previously seen, find a better way to design this
+    recurr_list = {} # Emptying global cache of board states previously seen, find a better way to design this
     # root = generate_tree(0, None, cboard, None, depth)
     # stopwatch.start()
 
@@ -189,7 +107,7 @@ def minimax_recur(board, eval):
     test = recur_func_2([0, None, None], board.depth, board.cboard, eval, 0, float('-inf'), float('inf'))
     stopwatch.stop()
     print(str(stopwatch))
-    # print(RenderTree(test[2]))
+    # print(RenderTree(test[2], maxlevel=2))
     print(test[0], test[1])
     return board.cboard.san(test[1])
 
@@ -202,6 +120,60 @@ def first_move(cboard):
 def random_move(cboard):
     return cboard.san(random.choice(list(cboard.legal_moves)))
 
+
+piece_type_list = [chess.PAWN, chess.ROOK, chess.BISHOP, chess.KNIGHT, chess.QUEEN, chess.KING]
+def eval2(cboard):
+    # Determining whether or not checkmate or stalemate
+    check = 0
+    if cboard.is_checkmate():
+        if cboard.turn == chess.WHITE:
+            return -100
+        elif cboard.turn == chess.BLACK:
+            return 100
+    if cboard.is_stalemate():
+        return 0
+
+    # Small incentive to check the opponent
+    if cboard.is_check():
+        if cboard.turn == chess.WHITE:
+            check = -5
+        elif cboard.turn == chess.BLACK:
+            check = 5
+
+    bb_white_pieces = chess.SquareSet(chess.BB_ALL)
+    for piece_type in piece_type_list:
+        bb_white_pieces = bb_white_pieces | cboard.pieces(piece_type, chess.WHITE)
+
+    bb_black_pieces = chess.SquareSet(chess.BB_ALL)
+    for piece_type in piece_type_list:
+        bb_black_pieces = bb_black_pieces | cboard.pieces(piece_type, chess.BLACK)
+    
+    ally_multiplier = 1.2
+    opponent_multiplier = 1.1
+
+    value = 0
+
+    row = 56
+    column = 0
+    for char in cboard.board_fen():
+        if char != '/':
+            if char.isdigit():
+                column += int(char)
+            else:
+                if char.isupper():
+                    value += ally_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_white_pieces)))
+                    value += opponent_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_black_pieces)))
+                elif char.islower():
+                    value += ally_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_black_pieces)))
+                    value += opponent_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_white_pieces)))
+
+                value += (piece_value[char] * (len(cboard.attacks(row+column) | (~bb_white_pieces & ~bb_black_pieces))))
+                column += 1
+        else:
+            row -= 8
+            column = 0
+
+    return value + check
 
 counter=0 
 # Naive evaluation function (piece capture, checkmate)
@@ -231,6 +203,6 @@ def naive_eval(cboard):
 
 # # MAIN
 board = Board(chess.WHITE)
-board.cboard = chess.Board("rnbqkbnr/pppppppp/6N1/8/8/8/PPPPPPPP/RNBQKB1R w KQkq - 0 1")
+board.cboard = chess.Board("r1b1k1nr/1p1p1p2/2n1pqp1/p1b3B1/2B1P2p/2N2N2/PPP2PPP/R2QR1K1 w kq - 0 10")
 
-minimax_recur(board, naive_eval)
+minimax_recur(board, eval2)
--- test.py
+++ test.py
@@ -1,10 +1,97 @@
-def func(i):
-    print(i)
-    if i > 50:
-        return i
-    i = i+10
-    func(i+10)
+import chess
+
+piece_value = {
+    'P' : 1,
+    'N' : 3,
+    'B' : 3,
+    'R' : 5,
+    'Q' : 9,
+    'K' : 0.5, # Kings will always be on the board
+    'p' : -1,
+    'n' : -3,
+    'b' : -3,
+    'r' : -5,
+    'q' : -9,
+    'k' : -0.5
+}
+
+piece_type_list = [chess.PAWN, chess.ROOK, chess.BISHOP, chess.KNIGHT, chess.QUEEN, chess.KING]
+
+def eval(cboard):
+    white_count = 0
+    black_count = 0
+
+    if cboard.is_checkmate():
+        if cboard.turn == chess.WHITE:
+            return -1000
+        elif cboard.turn == chess.BLACK:
+            return 1000
+    if cboard.is_stalemate():
+        return 0
+
+    board_fen = cboard.board_fen()
+    for char in board_fen:
+        if char.isupper():
+            white_count += piece_value[char]
+        elif char.islower():
+            black_count += piece_value[char]
+
+    material_balance = white_count + black_count
+
+def eval2(cboard):
+
+    # Determining whether or not checkmate or stalemate
+    check = 0
+    if cboard.is_checkmate():
+        if cboard.turn == chess.WHITE:
+            return -100
+        elif cboard.turn == chess.BLACK:
+            return 100
+    if cboard.is_stalemate():
+        return 0
+
+    # Small incentive to check the opponent
+    if cboard.is_check():
+        if cboard.turn == chess.WHITE:
+            check = -5
+        elif cboard.turn == chess.BLACK:
+            check = 5
+
+    bb_white_pieces = chess.SquareSet(chess.BB_ALL)
+    for piece_type in piece_type_list:
+        bb_white_pieces = bb_white_pieces | cboard.pieces(piece_type, chess.WHITE)
+
+    bb_black_pieces = chess.SquareSet(chess.BB_ALL)
+    for piece_type in piece_type_list:
+        bb_black_pieces = bb_black_pieces | cboard.pieces(piece_type, chess.BLACK)
     
-a = 10
-print(func(a))
-print(a)
+    ally_multiplier = 1.2
+    opponent_multiplier = 1.1
+
+    value = 0
+
+    row = 56
+    column = 0
+    for char in cboard.board_fen():
+        if char != '/':
+            if char.isdigit():
+                column += int(char)
+            else:
+                if char.isupper():
+                    value += ally_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_white_pieces)))
+                    value += opponent_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_black_pieces)))
+                elif char.islower():
+                    value += ally_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_black_pieces)))
+                    value += opponent_multiplier * (piece_value[char] * (len(cboard.attacks(row+column) | bb_white_pieces)))
+
+                value += (piece_value[char] * (len(cboard.attacks(row+column) | (~bb_white_pieces & ~bb_black_pieces))))
+                column += 1
+        else:
+            row -= 8
+            column = 0
+
+    return value + check
+
+board = chess.Board("2b1k1nr/3q1p1p/r2p1b2/pp2p1p1/1Pnp4/PN1QPPPP/1B1K2B1/1R4NR w k - 0 20")
+
+print(eval2(board))
